afcb56aac9325ded454a274e38a918cef1ebafed
Saranya0919 <saranya.seelaboina@gmail.com>
2025-02-20 11:18:04 +0530

    Add files via upload

---
+++ student_detalis.java
@@ -0,0 +1,6 @@
+public class student_detalis{
+       public static void main(String[] args){
+              System.out.print("my name is saranya\n");
+              System.out.print("i am from india");
+       }
+}

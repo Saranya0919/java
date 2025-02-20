39547c8b7253e71addd289a57996b6446a7060ba
Saranya0919 <saranya.seelaboina@gmail.com>
2025-02-20 11:17:07 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,15 @@
+
+public class datatype {
+              public static void main(String[ ] args){
+              int mynum=5;
+              float myfloat=5.99f;
+              char mychar='s';
+              boolean mybool=true;
+              String mystr="hello";
+              System.out.println(mynum);
+              System.out.println(myfloat);
+              System.out.println(mychar);
+              System.out.println(mybool);
+              System.out.println(mystr);
+              }
+}
